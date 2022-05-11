17a088247dcb15f12ce976fa21e3be95588df402
Uzair Aslam <uzair.aslam@invozone.com>
2022-05-11 17:02:14 +0500

    Initial setup

---
+++ lib/cutfood.ex
@@ -0,0 +1,9 @@
+defmodule Cutfood do
+  @moduledoc """
+  Cutfood keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+  """
+end
